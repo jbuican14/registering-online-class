d90d62d61ce36c0b90e244a6fed12263e3b349aa
Bew <Bew@sysops-MBP.broadband>
2020-07-15 12:05:13 +0100

    Adding Contact functionality usinge Nodemailer with gmail

--- README.md
+++ README.md
@@ -2,8 +2,23 @@ The demo project for registering online class
 - Tools 
     - Node.js 
     - Babel 
+    - jade & jade converter (http://html2jade.org/);
+    - Express.js
 
 
 - index.js
     - body-parser : now is part of express framework (not an independent module);
+- public 
+    -assets
+    -css
+        -bootstrap.min.css
+        -main.css
+    -js
+- views
+    -index.jade 
+    -layout.jade
+    -about.jade
+    -contact.jade
+    -classes.jade
+
     
--- index.js
+++ index.js
@@ -1,15 +1,64 @@
 "use strict";
 
-const express = require("express");
-const path  = require("path");
-const nodemailer = require("nodemailer");
+const express       = require("express");
+const nodemailer    = require("nodemailer");
+const path          = require("path");
+const { info }      = require("console");
+const application   = express();
 
-const application = express();
 application.use(express.json());
 application.use(express.urlencoded({
-    extended: falses
+    extended: false
 }));
-application.set("port", process.env.PORT || 3002);
-application.listen(application.get("port"), ()=> {
+
+application.set("views", path.join(__dirname, "views"));
+application.set("view engine", "jade");
+
+application.use(express.static(path.join(__dirname, 'public'))); 
+
+application.get("/", (req, res) => {
+    res.render('index', {title: "Welcome"});
+});
+application.get("/about", function(req, res){
+    res.render("about");
+});
+application.get("/contact", function(req, res){
+    res.render("contact");
+});
+application.post("/contact/send", function(req, res){
+    let transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 465,
+        secureConnection: false,// true for 465, false for other ports
+        auth: {
+            user: "[EMAIL@GMAIL.COM]",
+            pass: "[PASSWORD]", 
+        },
+    });
+
+    var mailConfig = {
+        from: "Class Support <classsupport@gmail.com",
+        to: "[EMAIL]@gmail.com",
+        subject : "Thank you for contacting us",
+        text: "You contact us with the following message : " + req.body.message,
+        html: "<p>You contact us with the following message :</p> <p>" + req.body.message + "</p>"
+    };
+    let info = transporter.sendMail(mailConfig, (err, info) => {
+        if(err) {
+            console.log(err);
+            res.redirect("/contact");
+        }else{
+            console.log("Message sent" + info.response);  
+            res.redirect("/");      
+        }
+    });
+});
+application.get("/classes", function(req, res){
+    res.render("classes");
+});
+
+
+application.set("port", process.env.PORT || 3002)
+            .listen(application.get("port"), ()=> {
     console.log(`Server runs at http://localhost:${application.get("port")}`);
 })
